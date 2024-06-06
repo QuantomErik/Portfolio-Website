8b903c593df66318c07b6f68e64f75808db1f99c
QuantomErik <ey222ci@student.lnu.se>
2024-06-06 22:13:01 +0200

    Testing.

--- src/components/startpage/startpage.jsx
+++ src/components/startpage/startpage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function StartPage() {
   return (
     <div className="start-page">
-      <h1>Welcome to My Portfolio</h1>
+      <h1>Welcome to My Portfolio testing</h1>
       <p>This is the start page of my portfolio website.</p>
       {/* <Link to="/about">About Me</Link> */}
     </div>
