0ac2c3a836d2574238f1d026b379955585df40eb
QuantomErik <ey222ci@student.lnu.se>
2024-06-19 01:53:54 +0200

    Test.

--- src/components/projects/ProjectCards.jsx
+++ src/components/projects/ProjectCards.jsx
@@ -24,7 +24,7 @@ const ProjectCards = () => {
       ]
     },
     {
-      title: "Project Two",
+      title: "Test",
       description: "Description for project two.",
       image: "src/components/projects/backend1.jpg",
       liveDemoLink: "#",
