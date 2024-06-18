82c8e60c3678b8cc1ba1fbd8c556f4f0874d4d54
QuantomErik <ey222ci@student.lnu.se>
2024-06-18 21:04:20 +0200

    change path for logo.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
-                    src="./logo2.webp"
+                    src="/logo2.webp"
                     alt="Your Company"
                   />
                 </div>
