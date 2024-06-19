cf65b125d67b6e19b22e5b5e1ed27767e8dcff4b
QuantomErik <ey222ci@student.lnu.se>
2024-06-19 14:02:02 +0200

    Update Project Cards.

--- src/components/projects/ProjectCards.jsx
+++ src/components/projects/ProjectCards.jsx
@@ -24,25 +24,25 @@ const ProjectCards = () => {
       ]
     },
     {
-      title: "Project Two",
-      description: "Description for project two.",
+      title: "Upcoming Project",
+      /* description: "Description for project two.", */
       image: "frontend.webp",
       liveDemoLink: "#",
       githubLink: "#",
       tools: [
-        { icon: <FaJsSquare />, name: 'JavaScript' },
-        { icon: <FaReact />, name: 'React' }
+        /* { icon: <FaJsSquare />, name: 'JavaScript' },
+        { icon: <FaReact />, name: 'React' } */
       ]
     },
     {
-      title: "Project Three",
-      description: "Description for project three.",
+      title: "Upcoming Project",
+      /* description: "Description for project three.", */
       image: "backend1.jpg",
       liveDemoLink: "#",
       githubLink: "#",
       tools: [
-        { icon: <FaNodeJs />, name: 'Node.js' },
-        { icon: <DiMongodb />, name: 'MongoDB' }
+       /*  { icon: <FaNodeJs />, name: 'Node.js' },
+        { icon: <DiMongodb />, name: 'MongoDB' } */
       ]
     },
     {
@@ -97,12 +97,12 @@ const ProjectCards = () => {
             <h3 className="font-bold text-2xl mb-2">{projects[1].title}</h3>
             <p className="text-white text-base">{projects[1].description}</p>
             <div className="flex space-x-4 mt-4">
-              <a href={projects[1].liveDemoLink} className="text-blue-500 hover:text-blue-700 font-bold">
+              {/* <a href={projects[1].liveDemoLink} className="text-blue-500 hover:text-blue-700 font-bold">
                 Live Demo
               </a>
               <a href={projects[1].githubLink} className="text-blue-500 hover:text-blue-700 font-bold">
                 GitHub
-              </a>
+              </a> */}
             </div>
             <div className="absolute top-4 right-4 flex flex-col space-y-2">
               {projects[1].tools.map((tool, index) => (
@@ -122,12 +122,12 @@ const ProjectCards = () => {
             <h3 className="font-bold text-2xl">{projects[2].title}</h3>
             <p className="text-white text-base">{projects[2].description}</p>
             <div className="flex space-x-4 mt-4">
-              <a href={projects[2].liveDemoLink} className="text-blue-500 hover:text-blue-700 font-bold">
+              {/* <a href={projects[2].liveDemoLink} className="text-blue-500 hover:text-blue-700 font-bold">
                 Live Demo
               </a>
               <a href={projects[2].githubLink} className="text-blue-500 hover:text-blue-700 font-bold">
                 GitHub
-              </a>
+              </a> */}
             </div>
             <div className="absolute top-4 right-4 flex flex-col space-y-2">
               {projects[2].tools.map((tool, index) => (
